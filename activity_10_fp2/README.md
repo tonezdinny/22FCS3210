124d7645035db6102d41425cd2fbe87cd67f2e6d
Thyago Mota <thyagomota@gmail.com>
2022-10-25 10:51:15 -0600

    updates

---
+++ activity_10_fp2/README.md
@@ -0,0 +1,17 @@
+* # Instructions
+
+To further practice list comprehension, implement the following functions in Scala:
+
+a) find all of the numbers from 1–1000 that are divisible by 8.
+
+b) find all of the numbers from 1–1000 that have a 6 in them.
+
+c) count the number of spaces in a (given) string.
+
+d) remove all of the vowels in a (given) string.
+
+e) find all of the words in a (given) string that are less than 5 letters.
+
+f) count the length of each word in a (given) string.
+
+g) use a nested list comprehension to find all of the numbers from 1–1000 that are divisible by any single digit besides 1 (2–9).
---
+++ activity_10_fp2/src/Examples.scala
@@ -0,0 +1,52 @@
+/*
+ * CS3210 - Principles of Programming Languages - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 10 - Functional Programming Examples
+ */
+
+object Examples {
+
+  // TODO: find all of the numbers from 1–1000 that are divisible by 8
+  def lc1() = {
+
+  }
+
+  // TODO: find all of the numbers from 1–1000 that have a 6 in them
+  def lc2() = {
+
+  }
+
+  // TODO: count the number of spaces in a (given) string
+  def lc3(str: String) = {
+
+  }
+
+  // TODO: remove all of the vowels in a (given) string
+  def lc4(str: String) = {
+
+  }
+  // TODO: find all of the words in a (given) string that are less than 5 letters
+  def lc5(str: String) = {
+
+  }
+
+  // TODO: count the length of each word in a (given) string
+  def lc6(str: String) = {
+
+  }
+
+  // TODO: use a nested list comprehension to find all of the numbers from 1–1000 that are divisible by any single digit besides 1 (2–9)
+  def lc7() = {
+
+  }
+
+  def main(args: Array[String]): Unit = {
+//    println(lc1().mkString(" "))
+//    println(lc2().mkString(" "))
+//    println(lc3("this is a string with   8 spaces!"))
+//    println(lc4("this string will have its vowels removed!"))
+//    println(lc5("words with less than five letters in this string!").mkString(" "))
+//    println(lc6("counting the length of each word in this string!").mkString(" "))
+//    println(lc7().mkString(" "))
+  }
+}
