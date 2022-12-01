e9f2a0a295d7093ffad3c9b8a19a49995eedacbf
Thyago Mota <thyagomota@gmail.com>
2022-12-01 10:33:48 -0700

    updates

---
+++ prg_04_elections_sim/src/elections.lua
@@ -0,0 +1,30 @@
+-- CS3210 - Principles of Programming Languages - Fall 2022
+-- Instructor: Thyago Mota
+-- Description: elections simulation with coroutines
+-- Student Name(s):
+
+PROPOSALS = 3
+VOTERS    = 1000000
+
+-- TODO #1: create a ballot and randomly assign 0 (no) or 1 (yes) to each proposal; then begin a timed loop of up to 10s (also random); at each iteration the function should yield; at the end of the loop, insert the ballot in the (given) ballot_box
+function vote(ballot_box)
+    
+end
+
+-- TODO #2: return a tally of the results as a percent of 1s (yes) that was given to each proposal
+function tally_results(ballot_box) 
+    
+end
+
+-- TODO #3: time when the election started
+
+-- TODO #4: create a ballot_box
+
+-- TODO #4 create an array of "vote" coroutines
+
+-- TODO #5: schedule the coroutines so each has a fair chance of running until completion
+
+-- TODO #6: display the results
+
+-- TODO #7: show how long the election took in seconds
+
